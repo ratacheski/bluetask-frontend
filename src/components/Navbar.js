127af9f2a03724950b640230a1d313f5e0ac4c3d
Rafael <rafaelratacheski@gmail.com>
2020-03-29 00:20:21 -0300

    Criação da Navbar do Bluetasks

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import NavbarItem from './NavbarItem';
+import {APP_NAME} from '../constants';
+
+class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [
+                {name: "Listar Tarefas", href: "/", active: true},
+                {name: "Nova Tarefa", href: "/form", active: false}
+            ]
+        }
+
+        this.onClickHandler = this.onClickHandler.bind(this);
+    }
+
+    onClickHandler(itemClicked) {
+        const items = [...this.state.items];
+        items.forEach(item => {
+            if (item.name === itemClicked.name) {
+                item.active = true;
+            } else {
+                item.active = false;
+            }
+        });
+        this.setState({items});
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <span className="navbar-brand mb-0 h1">{APP_NAME}</span>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className="navbar-nav mr-auto">
+                        {this.state.items.map(
+                            item => <NavbarItem
+                                key={item.name}
+                                item={item}
+                                onClick={this.onClickHandler}/>
+                        )}
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
